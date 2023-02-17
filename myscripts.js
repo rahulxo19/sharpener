509f4741431dbdd5020eb032784c795b5caccde8
rahulxo19 <rahulchhikara2001@gmail.com>
2023-02-17 13:45:12 +0530

    6th commit

--- myscripts.js
+++ myscripts.js
@@ -10,3 +10,23 @@ const oddItems = document.querySelectorAll('#items li:nth-child(odd)');
 oddItems.forEach(item => {
   item.style.backgroundColor = 'green';
 });
+
+var itemList = document.querySelector('#items');
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+
+itemList.children[1].style.backgroundColor = 'yellow';
+
+itemList.firstElementChild.textContent = 'Hello World1';
+itemList.lastElementChild.textContent = 'Hello World at last';
+itemList.previousElementSibling.style.backgroundColor = 'orange';
+
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id= 'hello1';
+newDiv.setAttribute('title','Hello div');
+var newDivText = document.createTextNode('hello World');
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
