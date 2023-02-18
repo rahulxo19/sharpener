0a002c42a43634fe7db02af68c43cd4aecd0bbf4
rahulxo19 <rahulchhikara2001@gmail.com>
2023-02-18 13:07:30 +0530

    13th commit

--- form.js
+++ form.js
@@ -3,6 +3,7 @@ var form = document.getElementById('my-form');
 
 form.addEventListener('submit',addUser);
 users.addEventListener('click',removeItem);
+users.addEventListener('click',editItem);
 
 function addUser(e){
     e.preventDefault();
@@ -12,17 +13,27 @@ function addUser(e){
     console.log(newEmail);
     var li = document.createElement('li');
     li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(newName + " " + newEmail));
+    li.appendChild(document.createTextNode(newName + " " + newEmail + " "));
     var deleteBtn = document.createElement('button');
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
   // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+  deleteBtn.appendChild(document.createTextNode('delete '));
+
+  li.appendChild(deleteBtn);
+
+  var edit = document.createElement('button');
+
+  // Add classes to del button
+  edit.className = 'btn btn-danger btn-sm float-right edit';
+
+  // Append text node
+  edit.appendChild(document.createTextNode('edit '));
 
   // Append button to li
-  li.appendChild(deleteBtn);
+  li.appendChild(edit);
 
   // Append li to list
   users.appendChild(li);
@@ -49,3 +60,25 @@ function removeItem(e) {
     }
   }
 }
+function editItem(e) {
+  if (e.target.classList.contains('edit')) {
+    
+      var li = e.target.parentElement;
+      var ul = li.parentElement;
+      var form = ul.previousElementSibling;
+      console.log(form);
+      var name = li.textContent.trim().split(' ')[0];
+      var editName = document.getElementById('name');
+      var editEmail = document.getElementById('email');
+      editName.value = li.textContent.trim().split(' ')[0];
+      editEmail.value = li.textContent.trim().split(' ')[1];
+
+      // remove the list item from the ul element
+      ul.removeChild(li);
+
+      // remove the corresponding object from the local storage using the name value as the key
+      localStorage.removeItem(name);
+    
+  }
+}
+
