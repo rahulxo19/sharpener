e7262c729663e98e06095d32fb77431862a151b2
rahulxo19 <rahulchhikara2001@gmail.com>
2023-02-20 10:30:14 +0530

    expense tracker added

---
+++ expense.js
@@ -0,0 +1,62 @@
+var forms = document.getElementById('forms');
+var items = document.getElementById('items');
+
+forms.addEventListener('submit',addExpense);
+items.addEventListener('click',removeItm);
+items.addEventListener('click',editItm);
+
+function addExpense(e){
+    e.preventDefault();
+    var price = document.getElementById('price').value;
+    console.log(price);
+    var category = document.getElementById('category').value;
+    console.log(category);
+    var desc = document.getElementById('desc').value;
+    console.log(desc);
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(price + "-" + category + "-" + desc));
+    var dlt = document.createElement('button');
+    dlt.className = 'delete';
+    li.appendChild(dlt);
+    dlt.appendChild(document.createTextNode('delete'));
+    var edit = document.createElement('button');
+    edit.className = 'edit';
+    edit.appendChild(document.createTextNode('edit'));
+    li.appendChild(edit);
+    items.appendChild(li);
+    let obj = {
+        p : price,
+        c : category,
+        d : desc
+    }
+    localStorage.setItem(desc,JSON.stringify(obj));
+}
+
+function removeItm(e){
+    if(e.target.classList.contains('delete')){
+        var li = e.target.parentElement;
+        var ul = li.parentElement;
+        ul.removeChild(li);
+        var desc = li.firstChild.textContent.trim().split('-')[2];
+        localStorage.removeItem(desc);
+    }
+
+}
+
+function editItm(e){
+    if(e.target.classList.contains('edit')){
+        var li = e.target.parentElement;
+        var ul = li.parentElement;
+        console.log(li);
+        var toEdit = li.firstChild.textContent.trim().split('-');
+        var price = document.getElementById('price');
+        var cat = document.getElementById('category');
+        var desc = document.getElementById('desc');
+        price.value = toEdit[0];
+        cat.value = toEdit[1];
+        desc.value = toEdit[2];
+        console.log(price);
+        localStorage.removeItem(desc.value);
+    }
+}
+
